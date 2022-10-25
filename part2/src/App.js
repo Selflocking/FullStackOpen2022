cbdc5471ff15b57d77f04a1c13ff12c13dbbc3fa
YunShu <im@yunshu.site>
2022-10-25 11:16:21 +0800

    2.5: separate module

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,40 +1,4 @@
-const Content = ({ parts }) => {
-  const total = parts.reduce(
-    (sum, part) => sum + part.exercises
-    , 0)
-
-  return (
-    <>
-      {parts.map((part) =>
-        <p key={part.id}>
-          {part.name} {part.exercises}
-        </p>
-      )}
-      <b>total of {total} exercises</b>
-    </>
-  )
-}
-const Header = ({ name }) => {
-  return (
-    <h3>{name}</h3>
-  )
-}
-
-const Course = ({ courses }) => {
-  return (
-    <>
-      {
-        courses.map(
-          course =>
-            <div key={course.id}>
-              <Header name={course.name} />
-              <Content parts={course.parts} />
-            </div>
-        )
-      }
-    </>
-  )
-}
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
