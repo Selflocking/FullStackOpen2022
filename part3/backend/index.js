ac7b4433a0a38ce738f594309b108bd65e9d8e46
YunShu <im@yunshu.site>
2023-11-10 22:57:48 +0800

    Middleware

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,88 +1,80 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.use(express.json())
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+app.use(express.json());
+app.use(requestLogger);
 
 let notes = require("./notes.json");
-// let notes = [
-//     {
-//         id: 1,
-//         content: "HTML is easy",
-//         date: "2022-05-30T17:30:31.098Z",
-//         important: true
-//     },
-//     {
-//         id: 2,
-//         content: "Browser can execute only Javascript",
-//         date: "2022-05-30T18:39:34.091Z",
-//         important: false
-//     },
-//     {
-//         id: 3,
-//         content: "GET and POST are the most important methods of HTTP protocol",
-//         date: "2022-05-30T19:20:14.298Z",
-//         important: true
-//     }
-// ]
 
-app.get('/', function (request, response) {
-    response.send("<h1>Haaa</h1>")
-})
+app.get("/", function (request, response) {
+  response.send("<h1>Haaa</h1>");
+});
 
 app.get("/api/notes", (request, response) => {
-    // response.send(JSON.stringify(notes))
-    response.json(notes)
-})
+  // response.send(JSON.stringify(notes))
+  response.json(notes);
+});
 
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id) // return NaN if not a number
-    const note = notes.find(note => note.id === id)
-    if (note) {
-        response.json(note)
-    } else {
-        response.statusMessage = `Not Found This Note`
-        response.status(404).end();
-    }
-})
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id); // return NaN if not a number
+  const note = notes.find((note) => note.id === id);
+  if (note) {
+    response.json(note);
+  } else {
+    response.statusMessage = `Not Found This Note`;
+    response.status(404).end();
+  }
+});
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
 
-    response.status(204).end()
-})
+  response.status(204).end();
+});
 
 const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
-        : 0
-    return maxId + 1
-}
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
 
-app.post('/api/notes', (request, response) => {
-    const body = request.body
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
 
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
 
-    const note = {
-        content: body.content,
-        important: body.important || false,
-        date: new Date(),
-        id: generateId(),
-    }
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  };
 
-    notes = notes.concat(note)
+  notes = notes.concat(note);
 
-    response.json(note)
-})
+  response.json(note);
+});
 
-const PORT = 3001
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const PORT = 3001;
 
 app.listen(PORT, () => {
-    console.log(`Server listen ${PORT} port`);
-})
-
+  console.log(`Server listen ${PORT} port`);
+});
