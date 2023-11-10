d20a4183ebed4f7a5e67f896bd3202862ccaa6ca
YunShu <im@yunshu.site>
2023-11-10 23:18:26 +0800

    3.8*: Phonebook backend step8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,8 +2,18 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
+morgan.token("data", (req, res) => {
+  if (req.method !== "POST") {
+    return "";
+  } else {
+    return JSON.stringify(req.body);
+  }
+});
+
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :data")
+);
 
 // 定义端口
 const POST = 3001;
