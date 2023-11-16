3f2b9185e02dbc367867fd60573a666d08cf44d1
YunShu <im@yunshu.site>
2023-11-16 11:13:07 +0800

    3.15: Phonebook database, step3 3.16: Phonebook database, step4 3.17*: Phonebook database, step5 3.18*: Phonebook database step6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
 const Phone = require("./models/phone");
+const phone = require("./models/phone");
 
 morgan.token("data", (req, res) => {
   if (req.method !== "POST") {
@@ -13,12 +14,28 @@ morgan.token("data", (req, res) => {
   }
 });
 
+const requestLogger = morgan(
+  ":method :url :status :res[content-length] - :response-time ms :data"
+);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
 app.use(cors());
 app.use(express.static("build/"));
 app.use(express.json());
-app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms :data")
-);
+app.use(requestLogger);
 
 // 定义端口
 const PORT = process.env.PORT || 3001;
@@ -29,20 +46,52 @@ app.get("/api/persons", (req, res) => {
   });
 });
 
-app.get("/api/persons/:id", (req, res) => {
-  Phone.findById(req.params.id).then((phone) => {
-    res.json(phone);
-  });
+app.get("/api/persons/:id", (req, res, next) => {
+  Phone.findById(req.params.id)
+    .then((phone) => {
+      res.json(phone);
+      if (phone) {
+        res.json(phone);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+app.put("/api/persons/:id", (req, res, next) => {
+  const body = req.body;
+  Phone.findOneAndUpdate(
+    { name: body.name },
+    { number: body.number },
+    {
+      returnDocument: "after",
+    }
+  )
+    .then((phone) => {
+      if (phone) {
+        res.json(phone);
+        res.status(200).end();
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 app.delete("/api/persons/:id", (req, res) => {
-  Phone.findByIdAndDelete(req.params.id).then((phone) => {
-    res.json(phone);
-    res.status(200).end();
-  });
+  Phone.findByIdAndDelete(req.params.id)
+    .then((phone) => {
+      res.status(204).end();
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
-app.post("/api/persons", (req, res) => {
+app.post("/api/persons", (req, res, next) => {
   const body = req.body;
 
   if (!body) {
@@ -67,7 +116,6 @@ app.post("/api/persons", (req, res) => {
     name: body.name,
     number: body.number,
   });
-
   phone.save().then((savedPhone) => {
     res.json(savedPhone);
   });
@@ -81,6 +129,9 @@ app.get("/api/info", (req, res) => {
   result = result.concat("\n" + date.toString());
   res.send(result);
 });
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
